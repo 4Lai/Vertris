f41fdc41b4187027cb32bc299fb7c00766040aae
4Lai <krystianzielinski2tor@gmail.com>
2024-03-14 14:48:35 +0100

    footer + newsletter/ mobiles

--- src/app/shared/components/footer-single-section/footer-single-section.component.ts
+++ src/app/shared/components/footer-single-section/footer-single-section.component.ts
@@ -1,14 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { FooterSingle } from '../../interfaces/footer-single';
 
 @Component({
   selector: 'app-footer-single-section',
   templateUrl: './footer-single-section.component.html',
-  styleUrls: ['./footer-single-section.component.scss']
+  styleUrls: ['./footer-single-section.component.scss'],
 })
-export class FooterSingleSectionComponent {
-// @Input() title: string = ''
-// @Input() subTitle: string = ''
-// @Input() link: string = ''
-@Input() array:FooterSingle[] = []
+export class FooterSingleSectionComponent implements OnInit {
+  @Input() array: FooterSingle[] = [];
+  ex: boolean = false;
+  windowWidth: number = 0;
+  shouldBeExpanded: boolean = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth <= 900) {
+      this.shouldBeExpanded = true;
+    }
+    if (this.windowWidth > 900) {
+      this.shouldBeExpanded = false;
+    }
+  }
+
+  ngOnInit(): void {
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth <= 900) {
+      this.shouldBeExpanded = true;
+    }
+    if (this.windowWidth > 900) {
+      this.shouldBeExpanded = false;
+    }
+  }
+
+  expand() {
+    this.ex = !this.ex;
+  }
 }
